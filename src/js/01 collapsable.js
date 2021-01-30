94470f3b97da01aedced2f10e161c5e97a201a6a
ctalloc <cecilia.tallo@gmail.com>
2021-01-30 12:16:41 +0100

    trying different formulas for js collapsables.

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -1,14 +1,44 @@
 "use strict";
-//solo afecta a diseña pq al efectar a legend solo afecta al primer hijo.
+/* 
+PRUEBA DE USAR ARRAY Y FOR FALLIDA (despliega y oculta todos a la vez)
 const legendDesignElement = document.querySelector(".legend__design");
 const legendRellenaElement = document.querySelector(".legend__rellena");
 const legendComparteElement = document.querySelector(".legend__comparte");
-const collapsableElement = document.querySelector(".collapsable");
+const collapsableElements = document.querySelectorAll(".collapsable");
 
-function handleCollapsable() {
-  collapsableElement.classList.toggle("collapsable--on");
+console.log(collapsableElements);
+
+function handleCollapsable(ev) {
+  for (const collapsableElement of collapsableElements) {
+    const collapse = ev.currentTarget;
+    console.log(collapse);
+    collapsableElement.classList.toggle("collapsable--on");
+  }
 }
 
 legendDesignElement.addEventListener("click", handleCollapsable);
 legendRellenaElement.addEventListener("click", handleCollapsable);
-legendComparteElement.addEventListener("click", handleCollapsable);
+legendComparteElement.addEventListener("click", handleCollapsable); */
+
+
+const legendDesignElement = document.querySelector(".legend__design");
+const legendRellenaElement = document.querySelector(".legend__rellena");
+const legendComparteElement = document.querySelector(".legend__comparte");
+
+const collapsableDesignElement = document.querySelector(".collapsableDesign");
+const collapsableRellenaElement = document.querySelector(".collapsableRellena");
+const collapsableComparteElement = document.querySelector(".collapsableComparte");
+
+function handleCollapsableDesign() {
+  collapsableDesignElement.classList.toggle("collapsable--on");
+}; 
+function handleCollapsableRellena() {
+  collapsableRellenaElement.classList.toggle("collapsable--on");
+} ;
+function handleCollapsableComparte() {
+  collapsableComparteElement.classList.toggle("collapsable--on");
+}
+
+legendDesignElement.addEventListener("click", handleCollapsableDesign);
+legendRellenaElement.addEventListener("click", handleCollapsableRellena);
+legendComparteElement.addEventListener("click", handleCollapsableComparte);
